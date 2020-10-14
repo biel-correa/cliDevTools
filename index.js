36dfa8c0a98701f7254e21308deed3801917729c
Bielzim0 <gabrielcorre67@hotmail.com>
2020-10-14 08:45:15 -0300

    topng

--- index.js
+++ index.js
@@ -13,15 +13,34 @@ if (command == 'convert64') {
     resize(args)
 }else if(command == 'help'){
     help()
-} else {
+}else if(command == 'svgtopng'){
+    topng()
+}else {
     console.log("Command not found".red)
     help()
+}
+
+function topng(){
+    let filename = args[1]
+    let path = args[1]
+    let sharp = require('sharp')
+
+    if (filename.slice(0,2) == '.\\' || filename.slice(0,2) == './') {
+        filename = filename.slice(2, filename.length)
+        filename = filename.slice(0, filename.length - 4)
+    }
+    sharp(path).png().toFile(filename + '.png').then(info => {
+        console.log('Image successfully created as: '.blue + filename + '.png')
+    }).catch(res =>{
+        console.log('Something went wrong'.red)
+    })
 }
 
 function help(){
     console.log('Existing commands:'.blue)
     console.log('convert64 [path-to-file]'.blue)
     console.log('resize [path-to-file] [width] [height]'.blue)
+    console.log('topng [path-to-file]'.blue)
 }
 
 function resize(args) {
